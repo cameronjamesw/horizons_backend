36e7ae9463c64969ef061c4e5e70c02f9dcb9175
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-05 18:50:05 +0000

    remove editable within post_count in categroy model

--- category/models.py
+++ category/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
-    post_count = models.IntegerField(default=0, editable=False)
+    post_count = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
