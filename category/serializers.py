7e8d7b6beaba1a49ec17023a8012be35c2c05c01
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-05 17:00:08 +0000

    create category serializer

--- category/serializers.py
+++ category/serializers.py
@@ -7,3 +7,12 @@ class CategorySerializer(serializers.ModelSerializer):
     def get_is_admin(self, obj):
         superusers = User.objects.filter(is_superuser=True)
         return obj.owner in superusers
+
+    class Meta:
+        model = Category
+        fields = [
+            'id',
+            'name',
+            'post_count',
+            'created_at',
+        ]
