f938dc67d98d38d9658e22c4d42cd5fc05179484
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-06 21:37:54 +0000

    create category detail view

--- category/urls.py
+++ category/urls.py
@@ -3,4 +3,5 @@ from category import views
 
 urlpatterns = [
     path('categories/', views.CategoryList.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetail.as_view()),
 ]
--- category/views.py
+++ category/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, permissions
 from horizons_backend.permissions import IsAdminUserOrReadOnly
 from .models import Category
 from .serializers import CategorySerializer
@@ -15,3 +15,8 @@ class CategoryList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
