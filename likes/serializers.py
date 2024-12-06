fe68e908d6e9d887c2c1c7de661846b4991a235a
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-06 19:51:09 +0000

    create like serializer.py

---
+++ likes/serializers.py
@@ -0,0 +1,14 @@
+from django.rest_framework import serializers
+from .models import Like
+
+class LikeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Like
+        fields = [
+            'id',
+            'owner',
+            'post',
+            'created_at',
+        ]
