085a9eaa390e3e98bf4ceb4513fad995878249c3
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-10 17:20:29 +0000

    add django-filters, add filter fields

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, generics, filters
 from rest_framework.response import Response
 from .models import Post
@@ -21,7 +22,16 @@ class PostList(generics.ListCreateAPIView):
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
+        DjangoFilterBackend
     ]
+
+    filterset_fields = [
+        'owner__followed__owner__profile',
+        'likes__owner__profile',
+        'owner__profile',
+        'category',
+    ]
+
     ordering_fields = [
         'likes_count',
         'comments_count',
