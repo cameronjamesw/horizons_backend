4dccd07606f9b2922144b412c26f7e89cbd41cf3
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-10 13:55:37 +0000

    add following_id feild to followerSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from followers.models import Follower
 from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
     is_admin = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_admin(self, obj):
         superusers = User.objects.filter(is_superuser=True)
@@ -14,6 +16,15 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
@@ -29,4 +40,5 @@ class ProfileSerializer(serializers.ModelSerializer):
             'image',
             'is_owner',
             'is_admin',
+            'following_id',
         ]
