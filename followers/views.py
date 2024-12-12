b581948786e4310baca22bf61432263a918bb1ca
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-12 17:07:04 +0000

    add comments to followers code

--- followers/views.py
+++ followers/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Follower
 from .serializers import FollowerSerializer
-from horizons_backend.permissions import IsOwnerOrReadOnly
-
-# Create your views here.
+from horizons_backend.permissions import IsOwnerOrReadOnlys
 
 
 class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers, i.e. all instances of a user
+    following another user'.
+    Create a follower, i.e. follow a user if logged in.
+    Perform_create: associate the current logged in user with a follower.
+    """
     serializer_class = FollowerSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Follower.objects.all()
@@ -17,6 +21,11 @@ class FollowerList(generics.ListCreateAPIView):
 
 
 class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower
+    No Update view, as we either follow or unfollow users
+    Destroy a follower, i.e. unfollow someone if owner
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = FollowerSerializer
     queryset = Follower.objects.all()
