50c326caae8ff83bab72d8beb4244155d46d5dcc
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-17 14:22:53 +0000

    fix settings.py error

--- horizons_backend/settings.py
+++ horizons_backend/settings.py
@@ -120,13 +120,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if "CLIENT_ORIGIN" in os.environ:
-    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-r"^https://.*\.codeinstitute-ide\.net$",
-r"^https://.*\.gitpod\.io$"
-]
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:    
+    CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.codeinstitute-ide\.net$",]
 
 CORS_ALLOW_CREDENTIALS = True
 
