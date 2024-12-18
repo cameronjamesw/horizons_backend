23294a0ce52dfc3cfd4053dbea7aecad41c159c2
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-18 17:05:37 +0000

    add is_admin field to serializer

--- horizons_backend/serializers.py
+++ horizons_backend/serializers.py
@@ -15,8 +15,9 @@ class CurrentUserSerializer(UserDetailsSerializer):
     """
     profile_id = serializers.ReadOnlyField(source='profile.id')
     profile_image = serializers.ReadOnlyField(source='profile.image.url')
+    is_admin = serializers.ReadOnlyField(source="profile.is_admin")
 
     class Meta(UserDetailsSerializer.Meta):
         fields = UserDetailsSerializer.Meta.fields + (
-            'profile_id', 'profile_image'
+            'profile_id', 'profile_image', 'is_admin'
         )
