7a257c1bb697993622a9efcb49fa284d9a2fb881
Cameron Wheatley <cameron_2000@live.co.uk>
2024-12-20 20:02:01 +0000

    add blank field to comment content in models.py

--- comments/models.py
+++ comments/models.py
@@ -12,7 +12,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    content = models.TextField(blank=False)
 
     class Mta:
         ordering = ['-created_at']
